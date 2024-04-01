20e4873b6e4106701d55ba55e9eaac790dde9cc2
mangelperis <mapi_meliana@hotmail.com>
2024-04-01 04:41:42 +0200

    Fix cannot autowire BookingRepository.php

--- app/src/Domain/Repository/BookingRepository.php
+++ app/src/Domain/Repository/BookingRepository.php
@@ -5,11 +5,20 @@ declare(strict_types=1);
 namespace App\Domain\Repository;
 
 use App\Domain\Entity\Booking;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Exception;
 
 class BookingRepository extends EntityRepository implements BookingRepositoryInterface
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct($entityManager, $entityManager->getClassMetadata(Booking::class));
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @param Booking $booking
      * @return bool
@@ -17,8 +26,8 @@ class BookingRepository extends EntityRepository implements BookingRepositoryInt
     public function save(Booking $booking): bool
     {
         try {
-            $this->getEntityManager()->persist($booking);
-            $this->getEntityManager()->flush();
+            $this->entityManager->persist($booking);
+            $this->entityManager->flush();
             return true;
         } catch (Exception $exception) {
             return false;
