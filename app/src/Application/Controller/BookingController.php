8f061c9fd3633d7ef77483a49a21d20febf95368
mangelperis <mapi_meliana@hotmail.com>
2024-04-02 00:59:28 +0200

    Refactor BookingController.php with check that required params are received

--- app/src/Application/Controller/BookingController.php
+++ app/src/Application/Controller/BookingController.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Application\Controller;
 
 use App\Application\Service\BookingService;
+use App\Application\Service\Handler\ResponseHandler;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
@@ -14,19 +16,24 @@ class BookingController extends AbstractFOSRestController
 {
     const ROOM_PARAM_NAME = 'room';
     const HOTEL_PARAM_NAME = 'hotel';
-    public function __construct(private BookingService $bookingService)
+    public function __construct(private readonly BookingService $bookingService, private ResponseHandler $responseHandler)
     {
+
     }
 
     /**
      * @throws TransportExceptionInterface
      */
-    #[Route('/call', name: 'call')]
+    #[Route('/booking', name: 'get_booking_by_room_and_hotel', methods: ['GET'])]
 
-    public function call(Request $request)
+    public function call(Request $request): \Symfony\Component\HttpFoundation\JsonResponse
     {
         $roomNumber = $request->query->get(self::ROOM_PARAM_NAME);
         $hotelId = $request->query->get(self::HOTEL_PARAM_NAME);
+
+        if(null === $roomNumber || null === $hotelId){
+            return $this->responseHandler->createErrorResponse('Required parameter is missing', Response::HTTP_BAD_REQUEST);
+        }
 
         return $this->bookingService->run($hotelId, $roomNumber);
     }
